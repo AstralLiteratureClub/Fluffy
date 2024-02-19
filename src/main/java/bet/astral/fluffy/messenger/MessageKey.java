aee2b473c05fa5f2ebd5825aa2016ff5ce845aab
Antritus <antt.graft@gmail.com>
2024-02-19 19:41:33 +0200

    Improve how the messages are managed and improve the message names.

---
+++ src/main/java/bet/astral/fluffy/messenger/MessageKey.java
@@ -0,0 +1,134 @@
+package bet.astral.fluffy.messenger;
+
+import bet.astral.messenger.Messenger;
+import bet.astral.messenger.placeholder.Placeholder;
+import org.bukkit.Material;
+import org.jetbrains.annotations.NotNull;
+
+import java.lang.annotation.*;
+import java.lang.reflect.Field;
+import java.util.Map;
+
+public final class MessageKey
+{
+	/**
+	 * Loads all the messages inside this class. This uses reflections so it may throw out errors.
+	 * @param messenger messenger to load the messages from
+	 */
+	public static void loadMessages(Messenger<?> messenger) {
+		for (Field field : MessageKey.class.getFields()){
+			try {
+				if (!field.isAnnotationPresent(KeyType.class)){
+					return;
+				}
+				KeyType type = field.getAnnotation(KeyType.class);
+				field.setAccessible(true);
+				Object value = field.get(null);
+				if (value instanceof String key){
+					switch (type.value()){
+						case PLACEHOLDER -> {
+							Map<String, Placeholder> placeholders = messenger.loadPlaceholders(key);
+							if (placeholders.isEmpty()){
+								continue;
+							}
+							placeholders.forEach(messenger::overrideDefaultPlaceholder);
+						}
+						case MESSAGE -> {
+							messenger.loadMessage(key);
+						}
+					}
+					messenger.loadMessage(key);
+				}
+			} catch (IllegalAccessException e) {
+				throw new RuntimeException(e);
+			}
+
+		}
+	}
+	@KeyType(Type.PLACEHOLDER)
+	public static final String DEFAULT_PLACEHOLDER = "placeholders";
+
+	@KeyType(Type.MESSAGE)
+	public static final String COMBAT_ILLEGAL_COMMAND = "combat-execute-illegal-command";
+	@KeyType(Type.MESSAGE)
+	public static final String COMBAT_END = "combat-end";
+	/*
+	 * Rejoin - NPC
+	 */
+	@KeyType(Type.MESSAGE)
+	public static final String COMBAT_REJOIN_NPC_REPLACEMENT_ALIVE_BROADCAST = "combat-logged-rejoin-npc-replacement-alive-broadcast";
+	@KeyType(Type.MESSAGE)
+	public static final String COMBAT_REJOIN_NPC_REPLACEMENT_ALIVE = "combat-logged-rejoin-npc-replacement-alive-player";
+	@KeyType(Type.MESSAGE)
+	public static final String COMBAT_REJOIN_NPC_REPLACEMENT_DEAD_BROADCAST = "combat-logged-rejoin-npc-replacement-alive-broadcast";
+	@KeyType(Type.MESSAGE)
+	public static final String COMBAT_REJOIN_NPC_REPLACEMENT_DEAD = "combat-logged-rejoin-npc-replacement-alive-player";
+	/*
+	 * Rejoin - Kill
+	 */
+	@KeyType(Type.MESSAGE)
+	public static final String COMBAT_REJOIN_KILLED_BROADCAST = "combat-logged-rejoin-killed-broadcast";
+	@KeyType(Type.MESSAGE)
+	public static final String COMBAT_REJOIN_KILLED_PLAYER = "combat-logged-rejoin-killed-player";
+	/*
+	 * Rejoin - No action
+	 */
+	@KeyType(Type.MESSAGE)
+	public static final String COMBAT_REJOIN_NO_ACTION_BROADCAST = "combat-logged-rejoin-no-action-broadcast";
+	@KeyType(Type.MESSAGE)
+	public static final String COMBAT_REJOIN_NO_ACTION_PLAYER = "combat-logged-rejoin-no-action-player";
+	/*
+	 * Combat tagging
+	 */
+	@KeyType(Type.MESSAGE)
+	public static final String COMBAT_ENTER_VICTIM = "combat-enter-victim";
+	@KeyType(Type.MESSAGE)
+	public static final String COMBAT_ENTER_ATTACKER = "combat-enter-attacker";
+
+	/*
+	 * 1 second messages
+	 */
+	@KeyType(Type.MESSAGE)
+	public static final String COMBAT_1_SECOND_MESSAGE = "combat-every-1-second";
+	/*
+	 * Item restrictions
+	 */
+	@KeyType(Type.MESSAGE)
+	public static final String COMBAT_USE_ITEM_ELYTRA_GLIDE = "combat-use-item-elytra-glide";
+	@KeyType(Type.MESSAGE)
+	public static final String COMBAT_USE_ITEM_ELYTRA_ROCKET_BOOST = "combat-use-item-elytra-rocket-boost";
+	@KeyType(Type.MESSAGE)
+	public static final String COMBAT_USE_ITEM_TRIDENT_RIPTIDE = "combat-use-item-trident-riptide";
+	/*
+	 * Cooldowns - Item
+	 */
+	@KeyType(Type.MESSAGE)
+	public static final String COMBAT_COOLDOWN_DEFAULT = "combat-item-cooldown-default";
+	@KeyType(Type.MESSAGE)
+	public static final String COMBAT_COOLDOWN_ITEM_SPECIFIC = "combat-item-cooldown-%item%";
+
+	@NotNull
+	public static String itemSpecificItemCooldown(@NotNull Material material){
+		return COMBAT_COOLDOWN_ITEM_SPECIFIC.replace("%item%", material.name().toLowerCase());
+	}
+
+	/*
+	 * Cooldowns - Command
+	 */
+
+
+	@KeyType(Type.MESSAGE)
+	public static final String COMBAT_COOLDOWN_COMMAND = "combat-command-cooldown";
+
+	@Documented
+	@Target(ElementType.FIELD)
+	@Retention(RetentionPolicy.RUNTIME)
+	public @interface KeyType {
+		Type value() default Type.MESSAGE;
+	}
+
+	public enum Type {
+		PLACEHOLDER,
+		MESSAGE
+	}
+}
