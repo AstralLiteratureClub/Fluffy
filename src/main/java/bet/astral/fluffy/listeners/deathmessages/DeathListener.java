c1ac56fa5173f0e23ab203825dcdc4939d2129f1
Antritus <antt.graft@gmail.com>
2024-04-11 21:29:40 +0300

    Change old statistic profiles to Account

--- src/main/java/bet/astral/fluffy/listeners/deathmessages/DeathListener.java
+++ src/main/java/bet/astral/fluffy/listeners/deathmessages/DeathListener.java
@@ -3,13 +3,11 @@ package bet.astral.fluffy.listeners.deathmessages;
 import bet.astral.fluffy.FluffyCombat;
 import bet.astral.fluffy.api.CombatTag;
 import bet.astral.fluffy.api.CombatUser;
-import bet.astral.fluffy.api.StatisticUser;
-import bet.astral.messenger.Message;
+import bet.astral.fluffy.statistic.Account;
 import bet.astral.messenger.Messenger;
 import bet.astral.messenger.placeholder.PlaceholderList;
 import bet.astral.messenger.utils.PlaceholderUtils;
 import net.kyori.adventure.text.logger.slf4j.ComponentLogger;
-import org.bukkit.Material;
 import org.bukkit.entity.Entity;
 import org.bukkit.entity.EntityType;
 import org.bukkit.entity.LivingEntity;
@@ -22,11 +20,8 @@ import org.bukkit.event.entity.PlayerDeathEvent;
 import org.bukkit.inventory.ItemStack;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
-import oshi.hardware.platform.linux.LinuxUsbDevice;
 
 import java.lang.reflect.Field;
-import java.util.LinkedList;
-import java.util.List;
 
 public class DeathListener implements Listener {
 	private final Field nameField;
@@ -56,7 +51,7 @@ public class DeathListener implements Listener {
 		final Player player = event.getPlayer();
 		final CombatTag tag = fluffy.getCombatManager().getLatest(player);
 		final CombatUser user = fluffy.getUserManager().getUser(player);
-		final StatisticUser statisticUser = fluffy.getStatisticManager().get(player);
+		final Account statisticUser = fluffy.getStatisticManager().get(player);
 		getLogger().info("Died due to " + damageEvent.getCause().name());
 
 		String messageKey;
